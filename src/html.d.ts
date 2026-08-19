1de6419abc324e8d7aa5c1231edddd99be1e8ce2
Yvee1 <stevenvdb@live.nl>
2026-08-19 11:00:02 +0200

    Remove all unneeded old files

--- src/html.d.ts
+++
@@ -1,6 +0,0 @@
-// allows importing html from TypeScript
-// source: https://medium.com/@sampsonjoliver/importing-html-files-from-typescript-bd1c50909992
-declare module '*.html' {
-	const value: string;
-	export default value
-}
--- src/map.ts
+++
@@ -1,7 +0,0 @@
-import { SidePanel } from './ui/side-panel';
-
-export interface Map {
-	name: string;
-	description: string;
-	sidePanel: SidePanel;
-}
